39ebb48a6e5992cad6a1142be2a9d6241157cda9
MohFahmi27 <mohammadfahmi417@gmail.com>
2020-07-29 14:52:41 +0800

    add exercise 05 solution and explanation.

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -32,12 +32,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        // TODO (01) Find the navController from myNavHostFragment
-        // Since we're using KTX, you can call this.findNavController
-        // TODO (02) Link the navController to our ActionBar
-        // By calling NavigationUI.setupActionBarWithNavController
+//        THIS PART IS TO FIND THE NAV CONTROLLER AS REFERENCED OF THE ACTION
+        val navController = this.findNavController(R.id.myNavHostFragment)
+//        THIS WILL ADD THE UP BUTTON TO ALL FRAGMENT IN ACTIVITY EXCEPT THE START POSITION FRAGMENT
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
-    // TODO (03) Override onSupportNavigateUp
-    // Find the navController and then call navController.navigateUp
+//    THIS WILL MAKE SURE THAT UP BUTTON WILL ACT THE SAME AS BACK BUTTON
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
+    }
+
 }
